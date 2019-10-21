bb1536224f61979b052285a1959a0932abbd37e9
elemesaiana <aiana.elemes5@gmail.com>
2019-10-21 21:53:11 +0600

    sqrt

--- sqrt.go
+++ sqrt.go
@@ -4,11 +4,14 @@ func Sqrt(nb int) int {
 
 	var res int
 	res = 0
-	for i := 1; i < nb; i++ {
+	for i := 0; i < nb; i++ {
 		res = i * i
 		if res == nb {
 			return i
 		}
+	}
+	if nb == 1 {
+		return 1
 	}
 	return 0
 }
