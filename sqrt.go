5942cfd97170f80725fe1dcc9c54ec255b86694e
elemesaiana <aiana.elemes5@gmail.com>
2019-10-21 21:45:46 +0600

    sqrt

--- sqrt.go
+++ sqrt.go
@@ -1,20 +1,13 @@
 package piscine
 
-import (
-	"math"
-)
-
 func Sqrt(nb int) int {
 
-	var a int
-	a = 0
-	for i := 1; i <= nb; i++ {
-		a = i * i
-
-		if a == nb {
-
-			a = int((math.Sqrt(float64(nb))))
-			return a
+	var res int
+	res = 0
+	for i := 1; i < nb; i++ {
+		res = i * i
+		if res == nb {
+			return i
 		}
 	}
 	return 0
