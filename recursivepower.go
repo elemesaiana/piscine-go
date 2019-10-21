4df02bbcd4e9fe9c93d2cd0786da0f32a2c6be3f
elemesaiana <aiana.elemes5@gmail.com>
2019-10-21 19:05:55 +0600

    quest-04.4

---
+++ recursivepower.go
@@ -0,0 +1,18 @@
+package piscine
+
+func RecursivePower(nb int, power int) int {
+
+	var res int
+	res = 1
+	if power > 0 {
+		for i := 1; i <= power; i++ {
+			res = res * nb
+		}
+	} else if power == 0 {
+		res = 1
+	} else {
+		res = 0
+	}
+
+	return res
+}
