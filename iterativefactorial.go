72dadd742d58d86c5459d732b5bc383dad82bd0f
elemesaiana <aiana.elemes5@gmail.com>
2019-10-21 17:21:05 +0600

    quest-04.1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -6,15 +6,14 @@ func IterativeFactorial(nb int) int {
 	for i := 0; i < nb+1; i++ {
 		if i == 0 || i == 1 {
 			res = 1
-		} else if res >= 2147483647 {
-			return 0
 
 		} else {
-
 			res = res * i
-
 		}
 
+		if res >= 2147483647 {
+			return 0
+		}
 	}
-	return 0
+	return res
 }
