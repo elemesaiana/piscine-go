09239718d49b329aafe428d6b80891b88b5e5729
elemesaiana <aiana.elemes5@gmail.com>
2019-10-21 19:58:33 +0600

    fib

--- fibonacci.go
+++ fibonacci.go
@@ -5,13 +5,19 @@ func Fibonacci(index int) int {
 	var f1, f2, fib int
 	f1 = 1
 	f2 = 1
-	fib = 0
-	for i := 3; i <= index; i++ {
-		fib = f1 + f2
-		f1 = f2
-		f2 = fib
+	fib = 1
+	if index > 0 {
+		for i := 3; i <= index; i++ {
+			fib = f1 + f2
+			f1 = f2
+			f2 = fib
 
+		}
+		return fib
+	} else if index == 0 {
+		return 0
+	} else {
+		return -1
 	}
 
-	return fib
 }
