a91e3d5558deb570260ee2d90e4226b11a0f5ba6
elemesaiana <aiana.elemes5@gmail.com>
2019-10-22 12:00:14 +0600

    alphacount.go

--- alphacount.go
+++ alphacount.go
@@ -1,16 +1,12 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
-
 func AlphaCount(str string) int {
 	counter := 0
 
 	for index, letter := range str {
 
-		if (letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122) {
-			z01.PrintRune(rune(index))
+		if (index == 1) || (letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122) {
+
 			counter++
 		}
 
