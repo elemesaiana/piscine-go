d149bde5294db49ada05341fa81c5080ed0a2d09
elemesaiana <aiana.elemes5@gmail.com>
2019-10-22 12:02:48 +0600

    alphacount.go

--- alphacount.go
+++ alphacount.go
@@ -5,7 +5,7 @@ func AlphaCount(str string) int {
 
 	for index, letter := range str {
 
-		if (index != 0) || (letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122) {
+		if (index >= 1) || (letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122) {
 
 			counter++
 		}
