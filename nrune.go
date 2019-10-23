936e47ae4d005b77e979a5e740171d06f717e41a
elemesaiana <aiana.elemes5@gmail.com>
2019-10-23 23:47:44 +0600

    nrune.go

--- nrune.go
+++ nrune.go
@@ -1,8 +1,16 @@
-package piscine
+package main
+
+import "github.com/01-edu/z01"
 
 func NRune(s string, n int) rune {
 	val := []rune(s)
-	n2 := n - 1
-	return val(n2)
+	v := n - 1
+	return val[v]
 
+}
+func main() {
+	z01.PrintRune(NRune("Hello!", 3))
+	z01.PrintRune(NRune("Salut!", 2))
+	z01.PrintRune(NRune("Ola!", 4))
+	z01.PrintRune('\n')
 }
