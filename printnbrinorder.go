81682f5e0a9229c40a80dfe2091b1da5c25b26b4
elemesaiana <aiana.elemes5@gmail.com>
2019-10-23 20:48:46 +0600

    printnbrinorder.go

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -1,35 +1,36 @@
-package piscine
+package main
 
 import (
-	"sort"
-
 	"github.com/01-edu/z01"
 )
 
 func PrintNbrInOrder(n int) {
 
-	var a, b, c int
 	var slice []int
 
 	if n >= 0 || n < 2147483647 {
 		if n == 0 {
 			z01.PrintRune(rune(n + 48))
 		} else {
-			a = n % 10
-			b = n / 10 % 10
-			c = n / 100 % 10
-			slice = append(slice, c, b, a)
-
-			sort.Ints(slice)
-
-			for _, digit := range slice {
-
-				z01.PrintRune(rune(digit + 48))
-
+			for n > 0 {
+				slice = append(slice, n%10)
+				n /= 10
 			}
 
 		}
 
+		for _, digit := range slice {
+
+			z01.PrintRune(rune(digit + 48))
+
+		}
+
 	}
+}
+
+func main() {
+	PrintNbrInOrder(321)
+	PrintNbrInOrder(0)
+	PrintNbrInOrder(321)
 
 }
