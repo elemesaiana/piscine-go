263afd60e7b2ae599123b13da38056a9781cf205
elemesaiana <aiana.elemes5@gmail.com>
2019-10-23 22:47:49 +0600

    firstrune.go

--- firstrune.go
+++ firstrune.go
@@ -4,7 +4,7 @@ func FirstRune(s string) rune {
 	v := ' '
 	for i, v := range s {
 
-		if (v >= 65 && v <= 90 && i == 0) || (v >= 97 && v <= 122 && i == 0) {
+		if v >= 0 && v <= 1000000000 && i == 0 {
 			return v
 
 		}
