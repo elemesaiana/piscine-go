d9ac66c691fd94ae1c34e229c1acd939a00117cd
elemesaiana <aiana.elemes5@gmail.com>
2019-10-23 21:48:59 +0600

    printnbrinorder.go

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -4,26 +4,40 @@ import (
 	"github.com/01-edu/z01"
 )
 
+func sort(mas []int) []int {
+	len := 0
+	for range mas {
+		len = len + 1
+	}
+	for i := len; i > 0; i-- {
+		for j := 1; j < i; j++ {
+			if mas[j] < mas[j-1] {
+				a := mas[j]
+				mas[j] = mas[j-1]
+				mas[j-1] = a
+			}
+		}
+	}
+	return mas
+}
+
 func PrintNbrInOrder(n int) {
 
 	var slice []int
+	if n == 0 {
+		z01.PrintRune(rune(0 + 48))
+	}
 
-	if n >= 0 || n < 2147483647 {
-		if n == 0 {
-			z01.PrintRune(rune(n + 48))
-		} else {
-			for n > 0 {
-				slice = append(slice, n%10)
-				n /= 10
-			}
+	for n > 0 {
 
-		}
+		slice = append(slice, n%10)
+		n /= 10
+	}
 
-		for _, digit := range slice {
+	for _, digit := range sort(slice) {
 
-			z01.PrintRune(rune(digit + 48))
-
-		}
+		z01.PrintRune(rune(digit + 48))
 
 	}
+
 }
